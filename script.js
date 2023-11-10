b45932cbce445dfa2021c0684108ecf0d9733666
woonzf <137765412+woonzf@users.noreply.github.com>
2023-11-10 19:00:46 +0800

    restructure code into module pattern, add functions: createGame, updateTurn, addResetListener and remove initializeGrid()

--- script.js
+++ script.js
@@ -1,51 +1,92 @@
 // Run after DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
-    // Cache DOM
-    const grid = document.querySelector(".grid");
-    const resetBtn = document.querySelector("#reset");
 
-    initializeGrid(grid, 9);
+    // Game module
+    const game = (function() {
 
-    resetBtn.addEventListener("click", () => {
-        while (grid.children.length > 0) grid.removeChild(grid.lastChild);
-        initializeGrid(grid, 9);
-    })
+        // Cache DOM
+        const grid = document.querySelector(".grid");
+        const resetBtn = document.querySelector("#reset");
+
+        // Initialize variables
+        const GRID_SIZE = 9;
+        const mark1 = 0;
+        const mark2 = 1;
+        let players;
+        let turn;
+
+        function createGame(name1, name2) {
+            const player1 = new Player(name1, mark1);
+            const player2 = new Player(name2, mark2);
+            players = [player1, player2];
+
+            // Determine player to start
+            turn = Math.floor(Math.random() * players.length);
+            
+            const boxes = createBoxes();
+
+            // Event Listeners
+            addBoxListener(boxes);
+            addResetListener(resetBtn, boxes);
+        }
+
+        function Player(name, mark) {
+            this.name = name;
+            this.mark = mark;
+        }
+
+        function updateTurn() {
+            turn = +!turn;
+        }
+        
+        function createBoxes() {
+            for (let i = 0; i < GRID_SIZE; i++) {
+                const box = document.createElement("div");
+                box.classList.add("box");
+                box.id = i + 1;
+                grid.append(box);
+            }
+        
+            return grid.querySelectorAll(".box");
+        }
+
+        function addBoxListener(boxes) {
+            boxes.forEach(box => {
+                box.addEventListener("click", placePlayerMark, { once:true });
+            })
+        }
+
+        function addResetListener(btn, boxes) {
+            btn.addEventListener("click", () => {
+                boxes.forEach(box => {
+                    while (box.children.length > 0) box.removeChild(box.lastChild);
+                    box.removeEventListener("click", placePlayerMark);
+                })
+
+                addBoxListener(boxes);
+            });
+        }
+        
+        function placePlayerMark(e) {
+            const player = players[turn];
+            const img = document.createElement("img");
+        
+            if (player.mark === mark1) {
+                img.src = "images/o.png";
+            }
+            else if (player.mark === mark2) {
+                img.src = "images/close.png";
+            }
+        
+            e.target.append(img);
+            updateTurn();
+        }
+
+        return { createGame };
+    })()
+
+    // Create game
+    const playerName1 = "Player 1";
+    const playerName2 = "Player 2";
+    game.createGame(playerName1, playerName2);
 })
-
-function initializeGrid(grid, number) {
-    const boxes = createBoxes(grid, number);
-    addBoxListener(boxes, "x");
-}
-
-function createBoxes(grid, number) {
-    for (let i = 0; i < number; i++) {
-        const box = document.createElement("div");
-        box.classList.add("box");
-        grid.append(box);
-    }
-
-    return grid.querySelectorAll(".box");
-}
-
-function addBoxListener(boxes, mark) {
-    boxes.forEach(box => {
-        box.addEventListener("click", () => {
-            placeMark(box, mark);
-        }, {
-            once: true
-        })
-    })
-}
-
-function placeMark(box, mark) {
-    const img = document.createElement("img");
-
-    if (mark === "x") {
-        img.src = "images/close.png";
-    }
-    else if (mark === "o") {
-        img.src = "images/o.png";
-    }
-
-    box.append(img);
-}
