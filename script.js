369d906c8a612e3d5fe106fb2537901892aa344e
woonzf <137765412+woonzf@users.noreply.github.com>
2023-11-10 12:36:53 +0800

    add functions: initializeGrid, createBoxes, addBoxListener and placeMark

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 // Run after DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
-    const boxes = document.querySelectorAll(".box");
+    // Cache DOM
+    const grid = document.querySelector(".grid");
+    const resetBtn = document.querySelector("#reset");
+
+    initializeGrid(grid, 9);
+
+    resetBtn.addEventListener("click", () => {
+        while (grid.children.length > 0) grid.removeChild(grid.lastChild);
+        initializeGrid(grid, 9);
+    })
+})
+
+function initializeGrid(grid, number) {
+    const boxes = createBoxes(grid, number);
+    addBoxListener(boxes, "x");
+}
+
+function createBoxes(grid, number) {
+    for (let i = 0; i < number; i++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        grid.append(box);
+    }
+
+    return grid.querySelectorAll(".box");
+}
+
+function addBoxListener(boxes, mark) {
     boxes.forEach(box => {
         box.addEventListener("click", () => {
-            const img = document.createElement("img");
-            img.src = "images/close.png";
-            box.append(img);
+            placeMark(box, mark);
         }, {
             once: true
         })
     })
-})
+}
+
+function placeMark(box, mark) {
+    const img = document.createElement("img");
+
+    if (mark === "x") {
+        img.src = "images/close.png";
+    }
+    else if (mark === "o") {
+        img.src = "images/o.png";
+    }
+
+    box.append(img);
+}
